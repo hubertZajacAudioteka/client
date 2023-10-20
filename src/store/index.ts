43db482bb845a612f1ddf8601a6e2c369a880c48
Hubert <hubert.zajac92@gmail.com>
2023-10-20 14:28:15 +0200

    Create store configuration for redux toolkit, persist redux and redux toolkit query

---
+++ src/store/index.ts
@@ -0,0 +1,52 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { userApi } from './apis/userApi';
+import userReducer from './slices/userSlice';
+import orderReducer from './slices/orderSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { orderApi } from './apis/orderApi';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['user', 'order'],
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  order: orderReducer,
+  [userApi.reducerPath]: userApi.reducer,
+  [orderApi.reducerPath]: orderApi.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+      .concat(userApi.middleware)
+      .concat(orderApi.middleware),
+});
+
+export const persistor = persistStore(store);
+
+setupListeners(store.dispatch);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
