401fcda2b26f40f430cdee83625db35f8c407f7b
Hubert <hubert.zajac92@gmail.com>
2023-10-20 14:51:41 +0200

    Add basic methods for user and order apis

---
+++ src/store/apis/orderApi.ts
@@ -0,0 +1,46 @@
+import {
+  AddOrderValues,
+  DeleteOrderResponse,
+  FetchClientResponse,
+  GetOrdersResponse,
+  Order,
+} from '@/types/order';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const orderApi = createApi({
+  reducerPath: 'orderApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}`,
+  }),
+  tagTypes: ['Order'],
+  endpoints(builder) {
+    return {
+      addOrder: builder.mutation<Order, AddOrderValues>({
+        query: (addOrderValues) => ({
+          url: '/orders',
+          method: 'POST',
+          body: addOrderValues,
+          credentials: 'include',
+        }),
+      }),
+      deleteOrder: builder.mutation<DeleteOrderResponse, string>({
+        query: (id) => ({
+          url: `/orders/${id}`,
+          method: 'DELETE',
+        }),
+        invalidatesTags: ['Order'],
+      }),
+      fetchSecretKey: builder.mutation<FetchClientResponse, AddOrderValues>({
+        query: (addOrderValues) => ({
+          url: `/orders/process`,
+          method: 'POST',
+          body: addOrderValues,
+          credentials: 'include',
+        }),
+        invalidatesTags: ['Order'],
+      }),
+    };
+  },
+});
+
+export const { useAddOrderMutation, useFetchSecretKeyMutation } = orderApi;
---
+++ src/store/apis/userApi.ts
@@ -0,0 +1,37 @@
+import {
+  UserFormLogin,
+  UserFormRegister,
+  UserLoginResponse,
+  UserRegisterResponse,
+} from '@/types/user';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const userApi = createApi({
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}`,
+  }),
+  tagTypes: ['User'],
+  endpoints(builder) {
+    return {
+      login: builder.mutation<UserLoginResponse, UserFormLogin>({
+        query: (userFormLogin) => ({
+          url: '/login',
+          method: 'POST',
+          body: userFormLogin,
+          credentials: 'include',
+        }),
+      }),
+      register: builder.mutation<UserRegisterResponse, UserFormRegister>({
+        query: (userFormRegister) => ({
+          url: '/register',
+          method: 'POST',
+          body: userFormRegister,
+          credentials: 'include',
+        }),
+      }),
+    };
+  },
+});
+
+export const { useLoginMutation, useRegisterMutation } = userApi;
