d21bf3eb9377a9a26dca86fa3a114ddae30cb08b
Hubert <hubert.zajac92@gmail.com>
2023-11-19 13:20:27 +0100

    Fix fetch errors

--- src/actions/order.ts
+++ src/actions/order.ts
@@ -4,32 +4,32 @@ import { cookies } from 'next/headers';
 
 export const getOrders = async (page: number) => {
   const jwt = cookies().get('jwt')?.value;
-  try {
-    const res = await fetch(`${process.env.API_URL}/orders?page=${page}`, {
-      headers: {
-        'content-type': 'application/json',
-        authorization: `Bearer ${jwt}`,
-      },
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+
+  const res = await fetch(`${process.env.API_URL}/orders?page=${page}`, {
+    headers: {
+      'content-type': 'application/json',
+      authorization: `Bearer ${jwt}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch orders');
   }
+  return res.json();
 };
 
 export const getOrder = async (id: string) => {
   const jwt = cookies().get('jwt')?.value;
-  try {
-    const res = await fetch(`${process.env.API_URL}/orders/${id}`, {
-      headers: {
-        'content-type': 'application/json',
-        authorization: `Bearer ${jwt}`,
-      },
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+
+  const res = await fetch(`${process.env.API_URL}/orders/${id}`, {
+    headers: {
+      'content-type': 'application/json',
+      authorization: `Bearer ${jwt}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch orders');
   }
+  return res.json();
 };
