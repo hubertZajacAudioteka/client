d6f9f74a8570ed835a332aeb7bfa02b19c42b33f
Hubert <hubert.zajac92@gmail.com>
2023-11-23 17:05:23 +0100

    Log error

--- src/actions/base.ts
+++ src/actions/base.ts
@@ -4,6 +4,37 @@ import { API_URL_SERVER } from '@/constants/api';
 import { Endpoint, GetRecordsByPageParams } from '@/types/serverSideRequest';
 import { getToken } from '@/utlis/getToken';
 
+// export const getRecordsByPageAction = async (
+//   endpoint: Endpoint,
+//   params: GetRecordsByPageParams
+// ) => {
+//   const jwt = getToken();
+
+//   let url = `${API_URL_SERVER}/${endpoint}?page=${params.page}`;
+
+//   for (const [key, value] of Object.entries(params)) {
+//     console.log('VALUE', value);
+//     if (value) {
+//       url += `&${key}=${value}`;
+//     }
+//   }
+//   const res = await fetch(url, {
+//     headers: {
+//       'content-type': 'application/json',
+//       authorization: `Bearer ${jwt}`,
+//     },
+//   });
+
+//   if (!res.ok) {
+//     throw new Error(`Failed to fetch`, {
+//       cause: {
+//         API_URL_SERVER,
+//       },
+//     });
+//   }
+//   return res.json();
+// };
+
 export const getRecordsByPageAction = async (
   endpoint: Endpoint,
   params: GetRecordsByPageParams
@@ -18,21 +49,19 @@ export const getRecordsByPageAction = async (
       url += `&${key}=${value}`;
     }
   }
-  const res = await fetch(url, {
-    headers: {
-      'content-type': 'application/json',
-      authorization: `Bearer ${jwt}`,
-    },
-  });
 
-  if (!res.ok) {
-    throw new Error(`Failed to fetch`, {
-      cause: {
-        API_URL_SERVER,
+  try {
+    const res = await fetch(url, {
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${jwt}`,
       },
     });
+
+    return res.json();
+  } catch (e) {
+    console.log(e);
   }
-  return res.json();
 };
 
 export const getRecordByIdAction = async (endpoint: Endpoint, id: string) => {
