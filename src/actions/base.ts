be476a24ec4627a7e1f4bebca528e6f4b44e15ae
Hubert <hubert.zajac92@gmail.com>
2023-11-23 18:03:38 +0100

    Code refactoring

--- src/actions/base.ts
+++ src/actions/base.ts
@@ -4,37 +4,6 @@ import { API_URL_SERVER } from '@/constants/api';
 import { Endpoint, GetRecordsByPageParams } from '@/types/serverSideRequest';
 import { getToken } from '@/utlis/getToken';
 
-// export const getRecordsByPageAction = async (
-//   endpoint: Endpoint,
-//   params: GetRecordsByPageParams
-// ) => {
-//   const jwt = getToken();
-
-//   let url = `${API_URL_SERVER}/${endpoint}?page=${params.page}`;
-
-//   for (const [key, value] of Object.entries(params)) {
-//     console.log('VALUE', value);
-//     if (value) {
-//       url += `&${key}=${value}`;
-//     }
-//   }
-//   const res = await fetch(url, {
-//     headers: {
-//       'content-type': 'application/json',
-//       authorization: `Bearer ${jwt}`,
-//     },
-//   });
-
-//   if (!res.ok) {
-//     throw new Error(`Failed to fetch`, {
-//       cause: {
-//         API_URL_SERVER,
-//       },
-//     });
-//   }
-//   return res.json();
-// };
-
 export const getRecordsByPageAction = async (
   endpoint: Endpoint,
   params: GetRecordsByPageParams
@@ -44,28 +13,21 @@ export const getRecordsByPageAction = async (
   let url = `${API_URL_SERVER}/${endpoint}?page=${params.page}`;
 
   for (const [key, value] of Object.entries(params)) {
-    console.log('VALUE', value);
     if (value) {
       url += `&${key}=${value}`;
     }
   }
+  const res = await fetch(url, {
+    headers: {
+      'content-type': 'application/json',
+      authorization: `Bearer ${jwt}`,
+    },
+  });
 
-  try {
-    const res = await fetch(url, {
-      headers: {
-        'content-type': 'application/json',
-        authorization: `Bearer ${jwt}`,
-      },
-    });
-
-    return res.json();
-  } catch (e) {
-    throw new Error('my', {
-      cause: {
-        e,
-      },
-    });
+  if (!res.ok) {
+    throw new Error(`Failed to fetch`);
   }
+  return res.json();
 };
 
 export const getRecordByIdAction = async (endpoint: Endpoint, id: string) => {
